77e55a6c30955f2a5da42f50a8faf9f068ebc664
vshighwire <158014598+vshighwire@users.noreply.github.com>
2024-05-06 09:07:46 +0000

    test commit

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,242 +1,41 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import Lottie from 'lottie-react';
+import animationData from './WelcomeAnimation.json'; // Import your Lottie animation JSON file
+ 
 function Welcome() {
+    const [isAnimationPlaying, setIsAnimationPlaying] = useState(true);
+ 
+    // Function to toggle animation playback
+    const toggleAnimation = () => {
+        setIsAnimationPlaying(!isAnimationPlaying);
+    };
+ 
     return (
-        <div>
-            <section
-                className="elementor-section elementor-top-section elementor-element elementor-element-7482b917 elementor-section-boxed elementor-section-height-default elementor-section-height-default"
-                data-id="7482b917"
-                data-element_type="section"
-                data-settings='{"background_background":"classic"}'
-            >
-                <div className="elementor-background-overlay" />
-                <div className="elementor-container elementor-column-gap-default">
-                    <div
-                        className="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-33272f24 elementor-invisible"
-                        data-id="33272f24"
-                        data-element_type="column"
-                        data-settings='{"animation":"fadeInLeft"}'
-                    >
-                        <div className="elementor-widget-wrap elementor-element-populated">
-                            <div
-                                className="elementor-element elementor-element-2e75e499 elementor-widget__width-auto elementor-widget elementor-widget-text-editor"
-                                data-id="2e75e499"
-                                data-element_type="widget"
-                                data-widget_type="text-editor.default"
-                            >
-                                <div className="elementor-widget-container">
-                                    <p>Welcome to Job Onboard</p>
-                                </div>
-                            </div>
-                            <div
-                                className="elementor-element elementor-element-2e56b1a2 elementor-widget elementor-widget-jkit_heading"
-                                data-id="2e56b1a2"
-                                data-element_type="widget"
-                                data-widget_type="jkit_heading.default"
-                            >
-                                <div className="elementor-widget-container">
-                                    <div className="jeg-elementor-kit jkit-heading  align-left align-tablet- align-mobile- jeg_module_61_1_6604237b1e2ee">
-                                        <div className="heading-section-title  display-inline-block">
-                                            <h2 className="heading-title">
-                                            Not sure how to start in your interview preparation?<br/>
-                                            We've got you covered.
-                                            </h2>
-                                        </div>
-                                        <div className="heading-section-description">
-                                            <p>
-                                            Unlock your career potential and empower your job search with our expert consultation, job hacks, and guaranteed interview calls.
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                className="elementor-element elementor-element-24862b4 elementor-widget__width-auto elementor-widget elementor-widget-button"
-                                data-id="24862b4"
-                                data-element_type="widget"
-                                data-widget_type="button.default"
-                            >
-                                <div className="elementor-widget-container">
-                                    <div className="elementor-button-wrapper">
-                                        <a
-                                            className="elementor-button elementor-button-link elementor-size-sm"
-                                            href="#"
-                                        >
-                                            <span className="elementor-button-content-wrapper">
-                                                <span className="elementor-button-text">
-                                                    Learn More
-                                                </span>
-                                            </span>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                className="elementor-element elementor-element-34b4a30 elementor-widget__width-auto elementor-widget elementor-widget-jkit_video_button"
-                                data-id="34b4a30"
-                                data-element_type="widget"
-                                data-widget_type="jkit_video_button.default"
-                            >
-                                <div className="elementor-widget-container">
-                                    <div
-                                        className="jeg-elementor-kit jkit-video-button jeg_module_61_2_6604237b1fd8f"
-                                        data-autoplay={0}
-                                        data-loop={0}
-                                        data-controls={0}
-                                        data-type="youtube"
-                                        data-mute={0}
-                                        data-start={0}
-                                        data-end={0}
-                                    >
-                                        <a href="#" className="jkit-video-popup-btn glow-enable">
-                                            <span className="icon-position-before">
-                                                <i aria-hidden="true" className="jki jki-play1-light" />
-                                            </span>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div
-                                className="elementor-element elementor-element-d890174 elementor-widget__width-initial elementor-hidden-mobile elementor-widget elementor-widget-heading"
-                                data-id="d890174"
-                                data-element_type="widget"
-                                data-widget_type="heading.default"
-                            >
-                                <div className="elementor-widget-container">
-                                    <h2 className="elementor-heading-title elementor-size-default">
-                                        Watch Video
-                                    </h2>
-                                </div>
-                            </div>
-                            <div
-                                className="elementor-element elementor-element-72de847f elementor-widget__width-auto elementor-widget elementor-widget-image"
-                                data-id="72de847f"
-                                data-element_type="widget"
-                                data-widget_type="image.default"
-                            >
-                                <div className="elementor-widget-container">
-                                    <img
-                                        width={300}
-                                        height={90}
-                                        src="https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/newclients-300x90.png"
-                                        className="attachment-medium size-medium wp-image-258"
-                                        alt=""
-                                        decoding="async"
-                                        loading="lazy"
-                                        srcSet="https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/newclients-300x90.png 300w, https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/newclients.png 321w"
-                                        sizes="(max-width: 300px) 100vw, 300px"
-                                    />
-                                </div>
-                            </div>
-                            <div
-                                className="elementor-element elementor-element-4f69e1ec elementor-widget__width-auto elementor-widget elementor-widget-heading"
-                                data-id="4f69e1ec"
-                                data-element_type="widget"
-                                data-widget_type="heading.default"
-                            >
-                                <div className="elementor-widget-container">
-                                    <h2 className="elementor-heading-title elementor-size-default">
-                                        2500+ Active Members
-                                    </h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div
-                        className="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-7cd3b263 elementor-invisible"
-                        data-id="7cd3b263"
-                        data-element_type="column"
-                        data-settings='{"animation":"fadeInRight"}'
-                    >
-                        <div className="elementor-widget-wrap elementor-element-populated">
-                            <div
-                                className="elementor-element elementor-element-64ad2b2 elementor-widget elementor-widget-image"
-                                data-id="64ad2b2"
-                                data-element_type="widget"
-                                data-widget_type="image.default"
-                            >
-                                <div className="elementor-widget-container">
-                                    <img
-                                        width={655}
-                                        height={708}
-                                        src="./WelcomeImg.jpg"
-                                        className="attachment-large size-large wp-image-5"
-                                        alt=""
-                                        decoding="async"
-                                        loading="lazy"
-                                        sizes="(max-width: 655px) 100vw, 655px"
-                                    />
-                                </div>
-                            </div>
-                            {/* <div
-                                className="elementor-element elementor-element-4d29022c elementor-widget elementor-widget-image"
-                                data-id="4d29022c"
-                                data-element_type="widget"
-                                data-widget_type="image.default"
-                            >
-                                <div className="elementor-widget-container">
-                                    <img
-                                        width={597}
-                                        height={1024}
-                                        src="https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/main2-597x1024.png"
-                                        className="attachment-large size-large wp-image-356"
-                                        alt=""
-                                        decoding="async"
-                                        loading="lazy"
-                                        srcSet="https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/main2-597x1024.png 597w, https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/main2-175x300.png 175w, https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/main2-768x1317.png 768w, https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/main2-896x1536.png 896w, https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/main2-1194x2048.png 1194w, https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/main2-800x1372.png 800w, https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/main2.png 1424w"
-                                        sizes="(max-width: 597px) 100vw, 597px"
-                                    />
-                                </div>
-                            </div> */}
-                            {/* <div
-                                className="elementor-element elementor-element-3b9bd8fb elementor-widget__width-initial elementor-absolute elementor-invisible elementor-widget elementor-widget-image"
-                                data-id="3b9bd8fb"
-                                data-element_type="widget"
-                                data-settings='{"_position":"absolute","_animation":"fadeInDown"}'
-                                data-widget_type="image.default"
-                            >
-                                <div className="elementor-widget-container">
-                                    <img
-                                        width={208}
-                                        height={209}
-                                        src="https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/jobcard.png"
-                                        className="attachment-medium_large size-medium_large wp-image-14"
-                                        alt=""
-                                        decoding="async"
-                                        loading="lazy"
-                                        srcSet="https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/jobcard.png 208w, https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/jobcard-150x150.png 150w"
-                                        sizes="(max-width: 208px) 100vw, 208px"
-                                    />
-                                </div>
-                            </div> */}
-                            {/* <div
-                                className="elementor-element elementor-element-3534614c elementor-widget__width-initial elementor-absolute elementor-hidden-mobile elementor-invisible elementor-widget elementor-widget-image"
-                                data-id="3534614c"
-                                data-element_type="widget"
-                                data-settings='{"_position":"absolute","_animation":"fadeInDown"}'
-                                data-widget_type="image.default"
-                            >
-                                <div className="elementor-widget-container">
-                                    <img
-                                        width={411}
-                                        height={161}
-                                        src="https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/Job-Card.png"
-                                        className="attachment-medium_large size-medium_large wp-image-13"
-                                        alt=""
-                                        decoding="async"
-                                        loading="lazy"
-                                        srcSet="https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/Job-Card.png 411w, https://advanture.icu/jobmart/wp-content/uploads/sites/8/2023/02/Job-Card-300x118.png 300w"
-                                        sizes="(max-width: 411px) 100vw, 411px"
-                                    />
-                                </div>
-                            </div> */}
-                        </div>
-                    </div>
-                </div>
-
-            </section>
-        </div>
-    )
+<div>
+<section>
+                {/* Your existing JSX code */}
+                {/* For example: */}
+<div>
+<p>Welcome to Job Onboard</p>
+</div>
+ 
+                {/* Add the Lottie animation */}
+<div>
+<Lottie
+                        animationData={animationData} // Pass in your animation JSON file
+                        loop={true} // Set loop to true if you want the animation to loop
+                        autoplay={isAnimationPlaying} // Control autoplay with state
+                        style={{ width: 300, height: 300 }} // Adjust size as needed
+                    />
+</div>
+ 
+                {/* Button to toggle animation playback */}
+<button onClick={toggleAnimation}>
+                    {isAnimationPlaying ? 'Pause Animation' : 'Play Animation'}
+</button>
+</section>
+</div>
+    );
 }
-
-export default Welcome
+ 
+export default Welcome;
